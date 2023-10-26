e7b3afc5e5ed8e6ce749d832f37a74fc21d655c3
IsrakAhmed <israkahmed7@gmail.com>
2023-10-26 23:37:33 +0600

    posts added & username field added in register

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,11 +28,30 @@
         <div class="col-9 pt-5">
             <div><h1>Israk Ahmed</h1></div>
             <div class="d-flex">
-                <div style="padding-right: 0.75rem;"><strong>153</strong> Posts</div>
-                <div style="padding-right: 0.75rem;"><strong>65k</strong> Followers</div>
-                <div style="padding-right: 0.75rem;"><strong>3</strong> Following</div>
+                <div style="padding-right: 1.25rem;"><strong>153</strong> Posts</div>
+                <div style="padding-right: 1.25rem;"><strong>65k</strong> Followers</div>
+                <div style="padding-right: 1.25rem;"><strong>3</strong> Following</div>
             </div>
+            <div class="pt-4" style="font-weight: bold;"><a style="color: black;" href="https://israkahmed.github.io/Portfolio">https://israkahmed.github.io/Portfolio</a></div>
+            <div>I'm a qualified and professional software developer with 3 years of experience in developing. I've strong creative and analytical skills. Team player with an eye for detail.</div>
+            <div><a style="color: black;" href="https://github.com/IsrakAhmed">https://github.com/IsrakAhmed</a></div>
         </div>
     </div>
+
+    <div class="row pt-5" style="text-align: center;">
+        <div class="col-4">
+        <img src="/post/inTajHat.jpg" height="350px">
+        </div>
+
+        <div class="col-4">
+        <img src="/post/durgaPuja2022.jpg" height="350px">
+        </div>
+        
+        <div class="col-4">
+        <img src="/post/icpc.jpg" height="350px">
+        </div>
+    </div>
+    
+
 </div>
 @endsection
