03a384f6be93eb2c4ef6f38d16b4c1677adba5cf
IsrakAhmed <israkahmed7@gmail.com>
2023-10-27 21:16:30 +0600

    profiles table created

--- database/migrations/2023_10_27_144144_create_profiles_table.php
+++ database/migrations/2023_10_27_144144_create_profiles_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->string('url')->nullable();
             $table->timestamps();
         });
     }
