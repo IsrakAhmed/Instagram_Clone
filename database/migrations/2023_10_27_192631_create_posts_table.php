0008957fcde9bcc8cd024deaeec6ab773dfe238e
IsrakAhmed <israkahmed7@gmail.com>
2023-10-28 01:43:31 +0600

    Post table created

--- database/migrations/2023_10_27_192631_create_posts_table.php
+++ database/migrations/2023_10_27_192631_create_posts_table.php
@@ -13,7 +13,14 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->string('caption');
+            $table->string('image');
+
             $table->timestamps();
+
+            $table->index('user_id');
         });
     }
 
