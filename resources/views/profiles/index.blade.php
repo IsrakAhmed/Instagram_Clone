7ccad60b6e95b690e74228f43c3fc0bb58820688
IsrakAhmed <israkahmed7@gmail.com>
2023-10-28 17:00:06 +0600

    route added to Add New Post button

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -28,7 +28,7 @@
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{$user->username}}</h1>
-                <a href="#">Add New Post</a>
+                <a href="/p/create">Add New Post</a>
             </div>
 
             <div class="d-flex">
