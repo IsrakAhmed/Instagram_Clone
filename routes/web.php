356192bb6b2395181458509b0eb0957c4d3760d1
IsrakAhmed <israkahmed7@gmail.com>
2023-10-29 13:39:18 +0600

    profile edit route added

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,4 @@ Route::get('/p/create', 'App\Http\Controllers\PostsController@create');
 Route::post('/p', 'App\Http\Controllers\PostsController@store');
 
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
+Route::get('/profile/{user}/edit', [App\Http\Controllers\ProfilesController::class, 'edit'])->name('profile.edit');
