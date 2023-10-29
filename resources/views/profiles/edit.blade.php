4243e3fa6d7274f94f5555796f17447806eda45b
IsrakAhmed <israkahmed7@gmail.com>
2023-10-29 14:11:14 +0600

    profile edit form added

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -11,14 +11,51 @@
             <div class="col-8 offset-2">
 
                 <div class="row">
-                    <h1>Add New Post</h1>
+                    <h1>Edit Profile</h1>
                 </div>
 
                 <div class="row mb-3">
-                    <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
+                    <label for="title" class="col-md-4 col-form-label">Title</label>
 
-                    <input id="caption" type="text" class="form-control @error('caption') is-invalid @enderror" name="caption" value="{{ old('caption') }}" autocomplete="caption" autofocus>
-                    @error('caption')
+                    <input id="title"
+                           type="text"
+                           class="form-control @error('title') is-invalid @enderror"
+                           name="title"
+                           value="{{ old('title') ?? $user->profile->title }}"
+                           autocomplete="title" autofocus>
+                    @error('title')
+                    <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                    @enderror
+                </div>
+
+                <div class="row mb-3">
+                    <label for="description" class="col-md-4 col-form-label">Description</label>
+
+                    <input id="description"
+                           type="text"
+                           class="form-control @error('description') is-invalid @enderror"
+                           name="description"
+                           value="{{ old('description') ?? $user->profile->description }}"
+                           autocomplete="description" autofocus>
+                    @error('description')
+                    <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                    @enderror
+                </div>
+
+                <div class="row mb-3">
+                    <label for="url" class="col-md-4 col-form-label">URL</label>
+
+                    <input id="url"
+                           type="text"
+                           class="form-control @error('url') is-invalid @enderror"
+                           name="url"
+                           value="{{ old('url') ?? $user->profile->url }}"
+                           autocomplete="url" autofocus>
+                    @error('url')
                     <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
@@ -26,7 +63,7 @@
                 </div>
 
                 <div class="row">
-                    <label for="image" class="col-md-4 col-form-label">Post Image</label>
+                    <label for="image" class="col-md-4 col-form-label">Profile Image</label>
 
                     <input type="file" class="form-control-file" id="image" name="image">
 
@@ -36,7 +73,7 @@
                 </div>
 
                 <div class="row pt-4">
-                    <button class="btn btn-primary">Add New Post</button>
+                    <button class="btn btn-primary">Save Profile</button>
                 </div>
 
             </div>
