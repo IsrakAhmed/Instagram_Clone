cf06205ee41945e749f8e7d585f3d2263d22fd13
IsrakAhmed <israkahmed7@gmail.com>
2023-11-08 18:57:24 +0600

    toggling following and unfollowing

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class FollowsController extends Controller
 {
-    //
+    public function store(User $user)
+    {
+        return auth()->user()->following()->toggle($user->profile);
+    }
 }
