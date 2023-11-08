6c738fe93e83e98b590825aeded823fea3175ae8
IsrakAhmed <israkahmed7@gmail.com>
2023-11-08 18:05:58 +0600

    fetched userId from index.blade

--- resources/js/components/FollowButton.vue
+++ resources/js/components/FollowButton.vue
@@ -1,13 +1,24 @@
 <template>
     <div>
-        <button class="btn btn-primary ms-4">Follow</button>
+        <button class="btn btn-primary ms-4" @click="followUser">Follow</button>
     </div>
 </template>
 
 <script>
     export default {
+
+        props: ['userId'],
+
         mounted() {
             console.log('Component mounted');
+        },
+        methods: {
+            followUser() {
+                axios.post('/follow/' + this.userId)
+                    .then(response => {
+                        alert(response.data);
+                    });
+            }
         }
     }
 </script>
