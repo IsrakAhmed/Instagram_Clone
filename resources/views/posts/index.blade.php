26503f08b022bdf3ce63eb8509e2b0a9fc37eca9
IsrakAhmed <israkahmed7@gmail.com>
2023-11-09 15:54:32 +0600

    index viewer added to show all posts

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    @foreach($posts as $post)
+        <div class="row">
+            <div class="col-6 offset-3">
+                <a href="/profile/{{ $post->user->id }}">
+                    <img src="/storage/{{ $post->image }}" class="w-75">
+                </a>
+            </div>
+        </div>
+        <div class="row pt-2 pb-4">
+            <div class="col-6 offset-3">
+                <div>
+                    <p>
+                    <span class="fw-bold">
+                        <a href="/profile/{{ $post->user->id }}" class="text-decoration-none">
+                            <span class="text-dark">{{ $post->user->username }}</span>
+                        </a>
+                    </span>
+                        {{ $post->caption }}
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
+@endsection
