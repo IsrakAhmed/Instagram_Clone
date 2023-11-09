2c611c9daaeb58351c7344b46db9781b6d3f8e95
IsrakAhmed <israkahmed7@gmail.com>
2023-11-09 19:17:29 +0600

    user can visit own profile from dropdown navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,6 +56,10 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ '/profile/' . Auth::user()->profile->user_id }}">
+                                        Profile
+                                    </a>
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
